4c7ab9bb790a92c548a28c016f294e1ef5530b7d
gurpreet562 <kgurpreet562@gmail.com>
2017-07-12 19:56:24 +0530

    uploaded assignment5.1
    uploaded assignment5.1 which depicts the variable declaration and variable output on the console.

---
+++ variable.js
@@ -0,0 +1,10 @@
+var age,name; 
+age=24; 
+name="Preeti" 
+var date1 = new Date();   
+date1.setFullYear(2011, 6, 1);   
+var pob="Kolkata" 
+document.write("<br><br>Your Name is:&nbsp"+name); 
+document.write("<br><br>Your age is:&nbsp"+age); 
+document.write("<br><br>Your Date of Birth is:&nbsp"+date1); 
+document.write("<br><br>Your Place of Birth is:&nbsp"+pob); 
